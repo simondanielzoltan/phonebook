dd7ca7724ecebf26374fca85517792fd7ab08406
Simon Dániel Zoltán <badsimon@hotmail.hu>
2023-03-22 12:38:58 +0100

    add main index blade template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
