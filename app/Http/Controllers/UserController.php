a248128be9632e682b6ddd37095a686a2ca46f19
Simon Dániel Zoltán <badsimon@hotmail.hu>
2023-03-22 12:37:37 +0100

    add store update destroy methods to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Email;
+use App\Models\Phone;
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
 
 class UserController extends Controller
 {
-    //
+    //TODO refactor store and update
+    public function store(UserCreateRequest $request) {
+        $user = User::create($request->only('name'));
+        $emails = [];
+        $phones = [];
+        if(isset($request->newEmails)){
+            foreach($request->newEmails as $email) {
+                $emails[] = new Email(['email' => $email]);
+            }
+        }
+        if(isset($request->newPhones)){
+            foreach($request->newPhones as $phone) {
+                $phones[] = new Phone(['phone_number' => $phone]);
+            }
+        }
+        $user->emails()->saveMany($emails);
+        $user->phones()->saveMany($phones);
+        return redirect('/user/' . $user->id);
+    }
+
+    public function update(UserUpdateRequest $request, User $user) {
+        $user->update($request->only('name'));
+        $emails = [];
+        $phones = [];
+        if(isset($request->newEmails)){
+            foreach($request->newEmails as $email) {
+                $emails[] = new Email(['email' => $email]);
+            }
+        }
+        if(isset($request->newPhones)){
+            foreach($request->newPhones as $phone) {
+                $phones[] = new Phone(['phone_number' => $phone]);
+            }
+        }
+        $user->emails()->saveMany($emails);
+        $user->phones()->saveMany($phones);
+        return redirect('/');
+    }
+
+    public function destroy(User $user) {
+        $user->delete();
+        return redirect('/');
+    }
 }
